d353f18947497b3509499e7c0b0a51a3c723877b
kkristianr <kristian.ristic@gmail.com>
2024-01-15 23:38:00 +0100

    Deploy last

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,14 +1,10 @@
-############ 1. IMPORTING LIBRARIES ############
-
-# Import streamlit, requests for API calls, and pandas and numpy for data manipulation
-
 import streamlit as st
 import requests
 import pandas as pd
 import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModel
-from streamlit_tags import st_tags  # to add labels on the fly!
+from streamlit_tags import st_tags  
 from torch import cuda
 device = 'cuda' if cuda.is_available() else 'cpu'
 
@@ -17,11 +13,12 @@ def load_model():
     model = torch.load('streamlit/finetuned/pytorch_model.bin', map_location=torch.device('cpu'))
     return model
 
-#model = torch.load('finetuned/pytorch_model.bin', map_location=torch.device('cpu'))
+##The class of the model is needed for loading the model
 class BERTMultiClass(torch.nn.Module):
     def __init__(self):
         super(BERTMultiClass, self).__init__()
-        self.l1 = AutoModel.from_pretrained(model_name)
+        #self.l1 = AutoModel.from_pretrained(model)
+        self.l1 = model_name
         self.pre_classifier = torch.nn.Linear(768, 768)
         self.dropout = torch.nn.Dropout(0.3)
         self.classifier = torch.nn.Linear(768, 12)
@@ -34,119 +31,99 @@ class BERTMultiClass(torch.nn.Module):
         pooler = torch.nn.ReLU()(pooler)
         pooler = self.dropout(pooler)
         output = self.classifier(pooler)
-        return output
+        return output 
+
+
+class_names = [
+    'bs',
+    'es-AR',
+    'es-ES',
+    'es-PE',
+    'hr',
+    'pt-BR',
+    'pt-PT',
+    'sr',
+    'BS',
+    'LU',
+    'ZH',
+    'BE'
+]
+
 
 def perform_inference(sample_sentence, model):
-    # Load pre-trained BERT model and tokenizer
     tokenizer = AutoTokenizer.from_pretrained("kkristianr/finetuned_bert_multilingual")
-
-    # Tokenize and prepare input for the model
     inputs = tokenizer(sample_sentence, return_tensors='pt', truncation=True, padding=True)
     input_ids = inputs['input_ids']
     attention_mask = inputs['attention_mask']
 
-    # Perform inference
     with torch.no_grad():
         model.eval()
         outputs = model(input_ids, attention_mask)
 
-    # Get probabilities
     probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    selected_probs = [probabilities[0, class_names.index(class_name)].item() for class_name in selected_classes]
 
-    # Convert probabilities to a list
-    probs_list = probabilities.squeeze().tolist()
+    #probs_list = probabilities.squeeze().tolist()
 
-    return probs_list
-
-
-############ 2. SETTING UP THE PAGE LAYOUT AND TITLE ############
-
-# `st.set_page_config` is used to display the default layout width, the title of the app, and the emoticon in the browser tab.
+    return selected_probs
 
 st.set_page_config(
     layout="centered", page_title="Dialect Classifier", page_icon="❄️"
 )
 
-############ CREATE THE LOGO AND HEADING ############
-
-# We create a set of columns to display the logo and the heading next to each other.
-
-
+## HEADER WITH TWO COLUMNS
 c1, c2 = st.columns([0.32, 2])
 
-# The snowflake logo will be displayed in the first column, on the left.
-
 with c1:
-
     st.image(
         "streamlit/images/loco.png",
+        #"images/loco.png",
         width=85,
     )
 
-
-# The heading will be on the right.
-
 with c2:
-
     st.caption("")
     st.title("Similar languages & dialects classifier")
-
-
-# We need to set up session state via st.session_state so that app interactions don't reset the app.
 
 if not "valid_inputs_received" in st.session_state:
     st.session_state["valid_inputs_received"] = False
 
-
-
-############ TABBED NAVIGATION ############
-
-# First, we're going to create a tabbed navigation for the app via st.tabs()
-# tabInfo displays info about the app.
-# tabMain displays the main app.
-
+## BODY CONTENT
 MainTab, InfoTab = st.tabs(["Main", "Info"])
 
 with InfoTab:
 
-    st.subheader("Why this app?")
+    st.subheader("What is this app?")
     st.markdown(
-        "[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python."
+        "This app shows the probabilities of a sentence being in a certain language or dialect. This is achieved by fine-tuning a multilingual BERT model on a dataset of 12 languages and dialects."
     )
 
     st.subheader("Resources")
     st.markdown(
         """
-    - [Streamlit Documentation](https://docs.streamlit.io/)
-    - [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
-    - [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
+    - [GitHub code](https://github.com/kkristianr/dialect-identification)
+    - [Finetuned tokenizer + model](https://huggingface.co/kkristianr/finetuned_bert_multilingual/)
     """
     )
 
 
 with MainTab:
     model = load_model()
-    # Then, we create a intro text for the app, which we wrap in a st.markdown() widget.
-
     st.write("")
     st.markdown(
-        """
-
+    """
     Distinguish between dialects and similar languages
-
     """
     )
+    selected_classes = st.multiselect("Select languages to include in the prediction:", class_names, default=class_names)
 
     st.write("")
     sample_sentence = st.text_input("Enter a sample sentence:", "Jelena moze pisati zadacu")
 
-
-# Perform BERT inference when the user clicks the button
-    if st.button("Run Inference"):
-        # Call the inference function
-        probs_list = perform_inference(sample_sentence, model)
-
-        # Display probabilities in Streamlit app
-        st.write("Probabilities for each class:")
-        for i, prob in enumerate(probs_list):
-            st.write(f"Class {i}: {prob:.4f}")
+# Call inference function
+    if st.button("Predict language"):
+        selected_probs = perform_inference(sample_sentence, model)
+        st.markdown("---")        
+        st.write("Probabilities for selected languages:")
+        table_data = {'Class': selected_classes, 'Probability': selected_probs}
+        st.table(table_data)
